420de1ca7a99b603287013707992dab843acf734
cloudy/cmetz <cometz@umich.edu>
2025-01-30 13:33:53 -0500

    wip

--- examples/amars_rt.cpp
+++ examples/amars_rt.cpp
@@ -1,5 +1,6 @@
 #include <sstream>
 #include <vector>
+#include <tuple>
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -40,9 +41,9 @@ disort::DisortOptions disort_options(int nwave, int ncol, int nlyr) {
   op.header("running amars RT");
   op.flags(
       "lamber,quiet,onlyfl,"
-      //"intensity_correction,old_intensity_correction");
-      "intensity_correction,old_intensity_correction,"
-      "print-input,print-phase-function");
+      "intensity_correction,old_intensity_correction");
+      //"intensity_correction,old_intensity_correction,"
+      //"print-input,print-phase-function");
 
   op.nwave(nwave);
   op.ncol(ncol);
@@ -92,11 +93,92 @@ torch::Tensor bb_toa_flux(torch::Tensor wave, int ncol, double temp, double fsca
   return bb_flux;
 }
 
+std::vector<std::vector<double>> read_4width_array_from_file(std::string fpath){
+  std::ifstream file(fpath);
+  std::vector<std::vector<double>> array_to_get;
+  std::string line;
+
+  std::getline(file, line); //skip the first line
+  while (std::getline(file, line)) {
+      std::istringstream iss(line);
+      std::vector<double> row;
+      double value;
+      while (iss >> value) {
+          row.push_back(value);
+      }
+      if (row.size() == 4) {
+          array_to_get.push_back(row);
+      } else {
+          std::cerr << "Invalid line format: " << line << std::endl;
+      }
+  }
+  file.close();
+
+  return array_to_get;
+}
+
+std::tuple<std::vector<double>, std::vector<double>, std::vector<std::vector<double>>> regrid_ptx(int nlyr, int nspecies, 
+  std::vector<double> p, std::vector<double> T, std::vector<std::vector<double>> mr) {
+  double p_min = *std::min_element(p.begin(), p.end());
+  double p_max = *std::max_element(p.begin(), p.end());
+  double p_step = (p_max - p_min) / (nlyr - 1);
+
+  double T_min = *std::min_element(T.begin(), T.end());
+  double T_max = *std::max_element(T.begin(), T.end());
+  double T_step = (T_max - T_min) / (nlyr - 1);
+
+  std::vector<double> new_p(nlyr);
+  std::vector<double> new_T(nlyr);
+  for (int i = 0; i < nlyr; ++i) {
+      new_p[nlyr - 1 - i] = p_min + i * p_step;
+      new_T[nlyr - 1 - i] = T_min + i * T_step;
+  }
+
+  std::vector<std::vector<double>> new_mr(nspecies, std::vector<double>(nlyr));
+  for (int j = 0; j < nspecies; ++j) {
+      harp::interpolate_mixing_ratios(p, mr[j], new_p, new_mr[j]);
+  }
+
+  return std::make_tuple(new_p, new_T, new_mr);
+}
+
+torch::Tensor calc_dz(int nlyr, torch::Tensor prop, const std::vector<double>& new_p, const std::vector<double>& new_rho, double g) {
+  auto dz = torch::ones({nlyr, 1}, prop.options()); // prop: (nwave, ncol, nlyr, nprop)
+  for (int i = 0; i < nlyr - 1; ++i) {
+    double dp = new_p[i] - new_p[i + 1];
+    dz[i][0] *= (dp) / (g * new_rho[i]);
+  }
+  // ignoring the p grid/z grid last layer issue. really p is in center and z is on the border
+  //we barely have any attenuration from the last layer anyway so just ignore this issue
+  dz[nlyr - 1][0] *= 2*dz[nlyr - 2][0];
+
+  return dz;
+}
+
+std::tuple<std::vector<std::vector<double>>, double, double> integrate_result(const torch::Tensor& result, const torch::Tensor& wave, int nlyr, int nwave) {
+  std::vector<std::vector<double>> integrated_flux(nlyr + 1, std::vector<double>(2, 0.0));
+  //assume a constant wavenumber grid
+  double d_lambda = wave[1].item<double>() - wave[0].item<double>();
+  double tot_flux_down_surf = 0;
+  double tot_flux_down_toa = 0;
+  for (int i = 0; i < nwave; ++i) {
+    for (int k = 0; k <= nlyr; ++k) {
+      integrated_flux[k][0] += result[i][0][k][0].item<double>() * d_lambda;  //result(x,x,x,0) is flux_up
+      integrated_flux[k][1] += result[i][0][k][1].item<double>() * d_lambda;  //1 is down
+    }
+    tot_flux_down_surf += result[i][0][0][1].item<double>() * d_lambda;
+    tot_flux_down_toa += result[i][0][nlyr][1].item<double>() * d_lambda;
+  }
+  
+  return std::make_tuple(integrated_flux, tot_flux_down_surf, tot_flux_down_toa);
+}
+
 int main(int argc, char **argv) {
-  int nwave = 5000; //near exact at TOA, and within 3 W/m^2 at surf from my initial test
-  //int nwave = 15000; essentially the exact value of the integral over the chosen wavelength bounds
+  //int nwave = 48;
+  int nwave = 500; //50 bins gets you within ~1 W/m^2 fldn at TOA, but we want to be sure to resolve spectral info
+  //int nwave = 15000; //essentially the exact value of the integral over the chosen wavelength bounds
   int ncol = 1;
-  int nlyr = 100; //100 layers is 3 W/m^2 away from the exact value when using 200 layers
+  int nlyr = 40; //3 layers is 1 W/m^2 away from the exact value of fldn_surf when using 200 layers. however, we want some more layers to resolve heating
   int nspecies = 2;
   double g = 3.711;
   double mean_mol_weight = 0.044; //CO2
@@ -109,28 +191,11 @@ int main(int argc, char **argv) {
 
   harp::S8Fuller s8(harp::S8RTOptions().species_id(0));
   harp::H2SO4Simple h2so4(harp::H2SO4RTOptions().species_id(1));
+  auto wave = short_wavenumber_grid(nwave);
+  auto conc = atm_concentration(ncol, nlyr, nspecies);
 
-  std::ifstream file("/home/cometz/pyharp/examples/aerosol_output_data.txt");
-  std::vector<std::vector<double>> aero_mr_p;
-  std::string line;
-
-  std::getline(file, line); //skip the first line
-  while (std::getline(file, line)) {
-      std::istringstream iss(line);
-      std::vector<double> row;
-      double value;
-      while (iss >> value) {
-          row.push_back(value);
-      }
-      if (row.size() == 4) {
-          aero_mr_p.push_back(row);
-      } else {
-          std::cerr << "Invalid line format: " << line << std::endl;
-      }
-  }
-  file.close();
-
-  // Extract pressure and mixing ratios
+  // read in the atmos output, and extract pressure and mixing ratios
+  std::vector<std::vector<double>> aero_mr_p = read_4width_array_from_file("/home/cometz/pyharp/examples/aerosol_output_data.txt");
   std::vector<double> p(aero_mr_p.size());
   std::vector<double> T(aero_mr_p.size());
   std::vector<std::vector<double>> mr(nspecies, std::vector<double>(aero_mr_p.size()));
@@ -141,32 +206,10 @@ int main(int argc, char **argv) {
           mr[j][i] = aero_mr_p[i][j + 2];
       }
   }
-
-  auto wave = short_wavenumber_grid(nwave);
-  auto conc = atm_concentration(ncol, nlyr, nspecies);
   
-  // Define the new pressure levels for interpolation
-  std::vector<double> new_p(nlyr);
-  std::vector<double> new_T(nlyr);
+  auto [new_p, new_T, new_mr] = regrid_ptx(nlyr, nspecies, p, T, mr);
   std::vector<double> new_rho(nlyr);
   std::vector<double> dT_ds(nlyr);
-  std::vector<std::vector<double>> integrated_flux(nlyr + 1, std::vector<double>(2, 0.0));
-  double p_min = *std::min_element(p.begin(), p.end());
-  double p_max = *std::max_element(p.begin(), p.end());
-  double p_step = (p_max - p_min) / (nlyr - 1);
-
-  double T_min = *std::min_element(T.begin(), T.end());
-  double T_max = *std::max_element(T.begin(), T.end());
-  double T_step = (T_max - T_min) / (nlyr - 1);
-  for (int i = 0; i < nlyr; ++i) {
-      new_p[nlyr - 1 - i] = p_min + i * p_step;
-      new_T[nlyr - 1 - i] = T_min + i * T_step;
-  }
-  
-  std::vector<std::vector<double>> new_mr(nspecies, std::vector<double>(nlyr));
-  for (int j = 0; j < nspecies; ++j) {
-      harp::interpolate_mixing_ratios(p, mr[j], new_p, new_mr[j]);
-  }
 
   for (int k = 0; k < nlyr; ++k) {
     conc[0][k][0] = (new_mr[1][k] * new_p[k]) / (R * new_T[k]); //s8 comes second in the file that we read in. but we need it to be index 0 in conc bc of how it was initialized above
@@ -176,17 +219,9 @@ int main(int argc, char **argv) {
 
   auto prop1 = s8->forward(wave, conc);
   auto prop2 = h2so4->forward(wave, conc);
-
   auto prop = prop1 + prop2;
-  // dz
-  auto dz = torch::ones({nlyr, 1}, prop.options()); // prop: (nwave, ncol, nlyr, nprop)
-  for (int i = 0; i < nlyr - 1; ++i) {
-    double dp = new_p[i] - new_p[i + 1];
-    dz[i][0] *= (dp) / (g * new_rho[i]);
-  }
-  // ignoring the p grid/z grid last layer issue. really p is in center and z is on the border
-  //we barely have any attenuration from the last layer anyway so just ignore this issue
-  dz[nlyr - 1][0] *= 2*dz[nlyr - 2][0];
+
+  auto dz = calc_dz(nlyr, prop, new_p, new_rho, g);
   prop *= dz;
 
   // mean single scattering albedo
@@ -198,23 +233,13 @@ int main(int argc, char **argv) {
   bc["albedo"] = 1.0 * torch::ones({nwave, ncol}, torch::kFloat64);
 
   auto result = disort->forward(prop, bc);
-  //std::cout << result << std::endl;
-  //assume a constant wavenumber grid
-  double d_lambda = wave[1].item<double>() -  wave[0].item<double>();
-  double tot_flux_down_surf = 0;
-  double tot_flux_down_toa = 0;
-  for (int i = 0; i < nwave; ++i) {
-    for (int k = 0; k <= nlyr; ++k) {
-      integrated_flux[k][0] += result[i][0][k][0].item<double>() * d_lambda;  //(x,x,x,0) is up
-      integrated_flux[k][1] += result[i][0][k][1].item<double>() * d_lambda;  //1 is down
-    }
-    tot_flux_down_surf += result[i][0][0][1].item<double>() * d_lambda;
-    tot_flux_down_toa += result[i][0][nlyr][1].item<double>() * d_lambda;
-  }
+
+  auto [integrated_flux, tot_flux_down_surf, tot_flux_down_toa] = integrate_result(result, wave, nlyr, nwave);
+
   std::cout << "tot_flux_down_surf: " << tot_flux_down_surf << " W/m^2" << std::endl;
   std::cout << "tot_flux_down_toa: " << tot_flux_down_toa << " W/m^2" << std::endl;
   
-
+  //calculate heating rates and write to file
   double df = 0;
   double df_iplus1 = 0;
   std::ofstream outputFile("dT_ds.txt");
@@ -227,7 +252,7 @@ int main(int argc, char **argv) {
   }
   outputFile.close();
 
-
+  //make sure the mixing ratio interpolation is working fine
   std::ofstream outputFile2("mix.txt");
   outputFile2 << "#p_new[Pa] mr(new)(0) mr(new)(1) p_atmos[Pa] mr(atmos)(0) mr(atmos)(1)" << std::endl;
   for (int k = 0; k < nlyr; ++k) {
