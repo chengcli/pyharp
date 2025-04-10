d1f53156e5960d8bacbd32a606e74c7d84cf9eb2
mac/cli <chengcli@umich.edu>
2025-04-10 10:52:15 -0400

    wip

--- cmake/pydisort.cmake
+++ cmake/pydisort.cmake
@@ -7,7 +7,7 @@ set_if_empty(TOKEN $ENV{GH_TOKEN})
 
 set(PACKAGE_NAME disort)
 set(REPO_URL "https://${ACCOUNT}:${TOKEN}@github.com/zoeyzyhu/pydisort")
-set(REPO_TAG "v0.10.3")
+set(REPO_TAG "v0.10.6")
 add_package(${PACKAGE_NAME} ${REPO_URL} ${REPO_TAG} "" ON)
 
 include_directories(${disort_SOURCE_DIR})
