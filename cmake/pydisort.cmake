f85fd4c8b0c8f7747cc1e3b2a340b38b937f732e
Cheng Li <69489965+chengcli@users.noreply.github.com>
2025-06-18 12:51:55 -0400

    Fix disort cmake (#58)

--- cmake/pydisort.cmake
+++ cmake/pydisort.cmake
@@ -3,7 +3,7 @@ set(FETCHCONTENT_QUIET TRUE)
 
 set(PACKAGE_NAME pydisort)
 set(REPO_URL "https://github.com/zoeyzyhu/pydisort")
-set(REPO_TAG "v1.2.2")
+set(REPO_TAG "v1.2.3")
 
 add_package(${PACKAGE_NAME} ${REPO_URL} ${REPO_TAG} "" ON)
 include_directories(${pydisort_SOURCE_DIR})
