bd43254f46b3de369e97c0c45c0bf908a8db05ed
Cheng Li <69489965+chengcli@users.noreply.github.com>
2025-06-26 13:00:49 -0400

    [#minor] Use shared library (#59)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+import sys
 import os
 import glob
 import torch
@@ -6,27 +6,34 @@ import platform
 from pathlib import Path
 from setuptools import setup
 from torch.utils import cpp_extension
+import sysconfig
 
 def parse_library_names(libdir):
     library_names = []
     for root, _, files in os.walk(libdir):
         for file in files:
-            if file.endswith((".a", ".so")):
+            if file.endswith((".a", ".so", ".dylib")):
                 file_name = os.path.basename(file)
                 library_names.append(file_name[3:].rsplit(".", 1)[0])
 
     # add system netcdf library
     library_names.extend(['netcdf'])
 
-    return sorted(library_names)
+    # move current library name to first
+    current = [item for item in library_names if item.startswith('harp')]
+    other = [item for item in library_names if not item.startswith('harp')]
+
+    return current + other
+
+site_dir = sysconfig.get_paths()["purelib"]
 
 current_dir = os.getenv("WORKSPACE", Path().absolute())
 include_dirs = [
     f"{current_dir}",
     f"{current_dir}/build",
     f"{current_dir}/build/_deps/fmt-src/include",
-    f'{current_dir}/build/_deps/pydisort-src',
     f'{current_dir}/build/_deps/yaml-cpp-src/include',
+    f"{site_dir}/pydisort",
 ]
 
 # add homebrew directories if on MacOS
@@ -38,10 +45,16 @@ else:
 
 libraries = parse_library_names(f"{current_dir}/build/lib")
 
-extra_link_args = []
-#if platform.system() == 'Linux':
-#    extra_link_args.extend(['-Wl,--whole-archive',
-#                            f"{current_dir}/build/lib/libharp_release.a", '-Wl,--no-whole-archive'])
+if sys.platform == "darwin":
+    extra_link_args = [
+        "-Wl,-rpath,@loader_path/lib",
+        "-Wl,-rpath,@loader_path/../torch/lib",
+    ]
+else:
+    extra_link_args = [
+        "-Wl,-rpath,$ORIGIN/lib",
+        "-Wl,-rpath,$ORIGIN/../torch/lib",
+    ]
 
 if torch.cuda.is_available():
     ext_module = cpp_extension.CUDAExtension(
@@ -68,7 +81,6 @@ else:
 
 setup(
     package_dir={"pyharp": "python"},
-    packages=["pyharp"],
     ext_modules=[ext_module],
     cmdclass={"build_ext": cpp_extension.BuildExtension},
 )
