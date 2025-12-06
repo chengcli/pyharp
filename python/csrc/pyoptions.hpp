4c59569f63fa7022fa6eecd7e3b2dafb5fcdac93
mac/cli <chengcli@umich.edu>
2025-12-05 20:08:29 -0500

    wip

--- python/csrc/pyoptions.hpp
+++ python/csrc/pyoptions.hpp
@@ -16,7 +16,9 @@
       .def_readonly("options", &harp::m_name##Impl::options) \
       .def("__repr__",                                       \
            [](const harp::m_name##Impl &a) {                 \
-             return fmt::format(#m_name "{}", a.options);    \
+             std::stringstream ss;                           \
+             a.options->report(ss);                          \
+             return fmt::format(#m_name "(\n{})", ss.str()); \
            })                                                \
       .def("module",                                         \
            [](harp::m_name##Impl &self, std::string name) {  \
