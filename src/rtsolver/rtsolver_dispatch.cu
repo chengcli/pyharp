dbd2f2e77c1bfd95f2f74519729b3bb5c106dca2
mac/cli <chengcli@umich.edu>
2026-01-18 13:46:33 -0500

    wip

--- src/rtsolver/rtsolver_dispatch.cu
+++ src/rtsolver/rtsolver_dispatch.cu
@@ -6,46 +6,57 @@
 #include <c10/cuda/CUDAGuard.h>
 
 // harp
-#include <disort/loops.cuh>
-#include "disort_dispatch.hpp"
-#include "disort_impl.h"
+#include <harp/loops.cuh>
+#include "rtsolver_dispatch.hpp"
+#include "toon_mckay89_longwave_impl.h"
+#include "toon_mckay89_shortwave_impl.h"
 
-namespace disort {
+namespace harp {
 
-void call_toon89_lw_cuda(at::TensorIterator& iter, int rank_in_column,
-                      disort_state *ds, disort_output *ds_out) {
+void call_toon89_sw_cuda(at::TensorIterator& iter) {
   at::cuda::CUDAGuard device_guard(iter.device());
 
-  AT_DISPATCH_FLOATING_TYPES(iter.dtype(), "call_disort_cuda", [&] {
-    auto nprop = at::native::ensure_nonempty_size(iter.output(), -1);
+  AT_DISPATCH_FLOATING_TYPES(iter.dtype(), "call_toon89_sw_cuda", [&] {
+    int nlay = at::native::ensure_nonempty_size(iter.input(1), -2);
+    int mem_size = toon89_sw_space<scalar_t>(nlay);
 
-    native::gpu_kernel<12>(
-        iter, [=] GPU_LAMBDA(char* const data[12], unsigned int strides[12]) {
+    native::gpu_mem_kernel<32, 5>(
+        iter, [=] GPU_LAMBDA(
+          char* const data[5], unsigned int strides[5], char *work) {
           auto out = reinterpret_cast<scalar_t*>(data[0] + strides[0]);
           auto prop = reinterpret_cast<scalar_t*>(data[1] + strides[1]);
           auto umu0 = reinterpret_cast<scalar_t*>(data[2] + strides[2]);
-          auto phi0 = reinterpret_cast<scalar_t*>(data[3] + strides[3]);
-          auto fbeam = reinterpret_cast<scalar_t*>(data[4] + strides[4]);
-          auto albedo = reinterpret_cast<scalar_t*>(data[5] + strides[5]);
-          auto fluor = reinterpret_cast<scalar_t*>(data[6] + strides[6]);
-          auto fisot = reinterpret_cast<scalar_t*>(data[7] + strides[7]);
-          auto temis = reinterpret_cast<scalar_t*>(data[8] + strides[8]);
-          auto btemp = reinterpret_cast<scalar_t*>(data[9] + strides[9]);
-          auto ttemp = reinterpret_cast<scalar_t*>(data[10] + strides[10]);
-          auto temf = reinterpret_cast<scalar_t*>(data[11] + strides[11]);
-          auto idxf = reinterpret_cast<scalar_t*>(data[12] + strides[12]);
-          int idx = static_cast<int>(*idxf);
-          //  disort_impl(out, prop, ftoa, temf, rank_in_column, ds[*idx],
-          //            ds_out[*idx], nprop);
+          auto fbeam = reinterpret_cast<scalar_t*>(data[3] + strides[3]);
+          auto albedo = reinterpret_cast<scalar_t*>(data[4] + strides[4]);
+          toon_mckay89_shortwave(nlay, *fbeam, umu0, prop, *albedo, out, work);
         });
   });
 }
 
-}  // namespace disort
+void call_toon89_lw_cuda(at::TensorIterator& iter) {
+  at::cuda::CUDAGuard device_guard(iter.device());
+
+  AT_DISPATCH_FLOATING_TYPES(iter.dtype(), "call_toon89_lw_cuda", [&] {
+    int nlay = at::native::ensure_nonempty_size(iter.input(1), -2);
+    int mem_size = toon89_sw_space<scalar_t>(nlay);
+
+    native::gpu_mem_kernel<32, 4>(
+        iter, [=] GPU_LAMBDA(
+          char* const data[4], unsigned int strides[4], char *work) {
+          auto out = reinterpret_cast<scalar_t*>(data[0] + strides[0]);
+          auto prop = reinterpret_cast<scalar_t*>(data[1] + strides[1]);
+          auto albedo = reinterpret_cast<scalar_t*>(data[2] + strides[2]);
+          auto be = reinterpret_cast<scalar_t*>(data[3] + strides[3]);
+          toon_mckay89_longwave(nlay, be, prop, *albedo, out, work);
+        });
+  });
+}
+
+}  // namespace harp
 
 namespace at::native {
 
-REGISTER_CUDA_DISPATCH(call_toon89_lw, &disort::call_toon89_lw_cuda);
-REGISTER_CUDA_DISPATCH(call_toon89_sw, &disort::call_toon89_sw_cuda);
+REGISTER_CUDA_DISPATCH(call_toon89_lw, &harp::call_toon89_lw_cuda);
+REGISTER_CUDA_DISPATCH(call_toon89_sw, &harp::call_toon89_sw_cuda);
 
 } // namespace at::native
